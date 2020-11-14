275bceb2873239e0631a673a4845c5e88970d4f6
jeshraghian <40262130+jeshraghian@users.noreply.github.com>
2020-11-15 08:49:33 +1100

    spike_train function added

--- snntorch/spikevision/spikegen.py
+++ snntorch/spikevision/spikegen.py
@@ -3,11 +3,40 @@ import torch
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
-def spike_conversion(data, targets, data_config, gain=1):
+def spike_conversion(data, targets, data_config, gain=1, convert_targets=True):
+    """Convert images into Poisson spike trains and their targets into one-hot spike trains. Input pixels are treated as
+    the probability for a spike to occur.
+
+               Parameters
+               ----------
+               data : torch tensor
+                   Input image data [batch size x channels x width x height].
+               targets : torch tensor
+                   Target tensor for a single minibatch.
+               data_config : snntorch configuration
+                   Configuration class.
+               gain: int, optional
+                    Factor to multiply the input tensor of pixel values for the Bernoulli distribution generator
+                    (default: ``1``).
+               convert_targets: Bool, optional
+                    Convert targets to time-domain one-hot-representation if True (default: ``True``).
+                Returns
+                -------
+                torch.Tensor
+                    spike train corresponding to input pixels.
+                torch.Tensor
+                    one hot encoding of targets with time in the first dimension.
+               """
+
     # Perform one-hot encoding of targets and repeat it along the time axis
-    spike_targets = targets_to_spikes(data_config, targets)
+    if convert_targets is True:
+        spike_targets = targets_to_spikes(data_config, targets)
+
+    else:
+        spike_targets = targets
 
     # Time x num_batches x channels x width x height
+    # This is only set-up for MNIST/single channel data. To-fix.
     time_data = data.repeat(data_config.T, 1, 1, 1, 1)*gain
 
     # Clip all gain between 0 and 1: these are treated as probabilities in the next line.
@@ -17,6 +46,21 @@ def spike_conversion(data, targets, data_config, gain=1):
     spike_data = torch.bernoulli(time_data)
 
     return spike_data, spike_targets
+
+
+# Use this function in spike_conversion
+def spike_train(N_in, data_config, rate):
+
+    # Time x number of neurons
+    time_data = torch.ones([data_config.T, N_in], device=device)*rate
+
+    # Clip all gain between 0 and 1: these are treated as probabilities in the next line.
+    time_data = torch.clamp(time_data, min=0, max=1)
+
+    # pass that entire time_data matrix into bernoulli.
+    spike_data = torch.bernoulli(time_data)
+
+    return spike_data
 
 
 def targets_to_spikes(data_config, targets):
@@ -81,3 +125,32 @@ def from_one_hot(one_hot_label):
        """
     one_hot_label = torch.where(one_hot_label == 1)[0][0]
     return int(one_hot_label)
+
+
+def spike_train(N_in, data_config, rate):
+
+    # if rate is a constant then just that number in the bernoulli dist
+    if not hasattr(rate, '__iter__'):
+        spike_in = torch.zeros([N_in])
+
+        # If rate is a scalar, fill all elements of spike_in with the rate
+        spike_in.fill_(rate)
+
+        # Time x N
+        spike_in = spike_in.repeat(data_config.T, 1)
+
+    # if rate defines one neurons behaviour: T x 1
+    elif rate[0] == data_config.T:
+        spike_in = rate.repeat(data_config.T, 1)
+
+    # Rate: T x N
+    else:
+        spike_in = rate
+
+    # Clip all gain between 0 and 1: these are treated as probabilities in the next line.
+    spike_in = torch.clamp(spike_in, min=0, max=1)
+
+    # pass that entire time_data matrix into bernoulli.
+    spike_in = torch.bernoulli(spike_in)
+
+    return spike_in
