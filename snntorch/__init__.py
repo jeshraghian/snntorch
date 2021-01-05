7adb58c80f8f9b9a1d62a9b2ebd6742721f5940d
jeshraghian <40262130+jeshraghian@users.noreply.github.com>
2021-01-05 19:58:01 +1100

    truncated BPTT methods; instance variable methods for Stein & SRM0 hidden states;

--- snntorch/__init__.py
+++ snntorch/__init__.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import weakref
 
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -55,6 +56,20 @@ class LIF(nn.Module):
         return spk, syn_pre, syn_post, mem
 
     @staticmethod
+    def detach(*args):
+        """Used to detach input arguments from the current graph.
+        Intended for use in truncated backpropagation through time where hidden state variables are global variables."""
+        for state in args:
+            state.detach_()
+
+    @staticmethod
+    def zeros(*args):
+        """Used to clear hidden state variables to zero.
+            Intended for use where hidden state variables are global variables."""
+        for state in args:
+            state = torch.zeros_like(state)
+
+    @staticmethod
     class Heaviside(torch.autograd.Function):
         """Default and non-approximate spiking function for neuron.
         Forward pass: Heaviside step function.
@@ -90,15 +105,62 @@ class Stein(LIF):
     R. B. Stein (1965) A theoretical analysis of neuron variability. Biophys. J. 5, pp. 173-194.
     R. B. Stein (1967) Some models of neuronal variability. Biophys. J. 7. pp. 37-68."""
 
-    def __init__(self, alpha, beta, threshold=1.0, spike_grad=None):
+    instances = []
+
+    def __init__(self, alpha, beta, threshold=1.0, num_inputs=False, spike_grad=None, batch_size=False, hidden_init=False):
         super(Stein, self).__init__(alpha, beta, threshold, spike_grad)
+        self.__class__.instances.append(weakref.proxy(self))
+
+        self.num_inputs = num_inputs
+        self.batch_size = batch_size
+        self.hidden_init = hidden_init
+
+        if self.hidden_init:
+            if not self.num_inputs:
+                raise ValueError("num_inputs must be specified to initialize hidden states as instance variables.")
+            elif not self.batch_size:
+                raise ValueError("batch_size must be specified to initialize hidden states as instance variables.")
+            elif hasattr(self.num_inputs, '__iter__'):
+                self.spk, self.syn, self.mem = self.init_stein(self.batch_size, *(self.num_inputs)) # need to automatically call batch_size
+            else:
+                self.spk, self.syn, self.mem = self.init_stein(self.batch_size, self.num_inputs)
+
+    def __del__(self):
+        return
 
     def forward(self, input_, syn, mem):
-        spk, reset = self.fire(mem)
-        syn = self.alpha * syn + input_
-        mem = self.beta * mem + syn - reset
+        if not self.hidden_init:
+            spk, reset = self.fire(mem)
+            syn = self.alpha * syn + input_
+            mem = self.beta * mem + syn - reset
 
-        return spk, syn, mem
+            return spk, syn, mem
+
+        # intended for truncated-BPTT where instance variables are hidden states
+        if self.hidden_init:
+            self.spk, self.reset = self.fire(self.mem)
+            self.syn = self.alpha * self.syn + input_
+            self.mem = self.beta * self.mem + self.syn - self.reset
+
+            return self.spk, self.syn, self.mem
+
+    @classmethod
+    def detach_hidden(cls):
+        """Used to detach hidden states from the current graph.
+        Intended for use in truncated backpropagation through time where hidden state variables are instance variables."""
+        for layer in range(len(cls.instances)):
+            cls.instances[layer].spk.detach_()
+            cls.instances[layer].syn.detach_()
+            cls.instances[layer].mem.detach_()
+
+    @classmethod
+    def zeros_hidden(cls):
+        """Used to clear hidden state variables to zero.
+        Intended for use where hidden state variables are instance variables."""
+        for layer in range(len(cls.instances)):
+            cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
+            cls.instances[layer].syn = torch.zeros_like(cls.instances[layer].syn)
+            cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
 
 
 class SRM0(LIF):
@@ -112,17 +174,48 @@ class SRM0(LIF):
     For further reading, see:
     R. Jovilet, J. Timothy, W. Gerstner (2003) The spike response model: A framework to predict neuronal spike trains. Artificial Neural Networks and Neural Information Processing, pp. 846-853.
     """
-    def __init__(self, alpha, beta, threshold=1.0, spike_grad=None):
+
+    instances = []
+
+    def __init__(self, alpha, beta, threshold=1.0, num_inputs=False, spike_grad=None, batch_size=False, hidden_init=False):
         super(SRM0, self).__init__(alpha, beta, threshold, spike_grad)
+        self.__class__.instances.append(self)
+
+        self.num_inputs = num_inputs
+        self.batch_size = batch_size
+        self.hidden_init = hidden_init
+
+        if self.hidden_init:
+            if not self.num_inputs:
+                raise ValueError("num_inputs must be specified to initialize hidden states as instance variables.")
+            elif not self.batch_size:
+                raise ValueError("batch_size must be specified to initialize hidden states as instance variables.")
+            elif hasattr(self.num_inputs, '__iter__'):
+                self.spk, self.syn_pre, self.syn_post, self.mem = self.init_srm0(batch_size=self.batch_size,
+                                                                                 *(self.num_inputs))
+            else:
+                self.spk, self.syn_pre, self.syn_post, self.mem = self.init_srm0(batch_size, num_inputs)
+
         self.tau_srm = np.log(self.alpha) / (np.log(self.beta) - np.log(self.alpha)) + 1
         if self.alpha <= self.beta:
             raise ValueError("alpha must be greater than beta.")
 
     def forward(self, input_, syn_pre, syn_post, mem):
-        spk, reset = self.fire(mem)
-        syn_pre = (self.alpha * syn_pre + input_) * (1 - reset)
-        syn_post = (self.beta * syn_post - input_) * (1 - reset)
-        mem = self.tau_srm * (syn_pre + syn_post)*(1-reset) + (mem*reset - reset)
+        # if hidden states are passed externally
+        if not self.hidden_init:
+            spk, reset = self.fire(mem)
+            syn_pre = (self.alpha * syn_pre + input_) * (1 - reset)
+            syn_post = (self.beta * syn_post - input_) * (1 - reset)
+            mem = self.tau_srm * (syn_pre + syn_post)*(1-reset) + (mem*reset - reset)
+            return spk, syn_pre, syn_post, mem
+
+        # if hidden states and outputs are instance variables
+        if self.hidden_init:
+            self.spk, self.reset = self.fire(self.mem)
+            self.syn_pre = (self.alpha * self.syn_pre + input_) * (1 - self.reset)
+            self.syn_post = (self.beta * self.syn_post - input_) * (1 - self.reset)
+            self.mem = self.tau_srm * (self.syn_pre + self.syn_post) * (1 - self.reset) + (self.mem * self.reset - self.reset)
+            return self.spk, self.syn_pre, self.syn_post, self.mem
 
     # cool forward function that resulted in burst firing - worth exploring
 
@@ -137,8 +230,27 @@ class SRM0(LIF):
     #     syn_post = self.beta * syn_post - input_
     #     mem = self.tau_srm * (syn_pre + syn_post) - reset
 
-        return spk, syn_pre, syn_post, mem
+        # return spk, syn_pre, syn_post, mem
 
+    @classmethod
+    def detach_hidden(cls):
+        """Used to detach hidden states from the current graph.
+        Intended for use in truncated backpropagation through time where hidden state variables are instance variables."""
+        for layer in range(len(cls.instances)):
+            cls.instances[layer].spk.detach_()
+            cls.instances[layer].syn_pre.detach_()
+            cls.instances[layer].syn_post.detach_()
+            cls.instances[layer].mem.detach_()
+
+    @classmethod
+    def zeros_hidden(cls):
+        """Used to clear hidden state variables to zero.
+        Intended for use where hidden state variables are instance variables."""
+        for layer in range(len(cls.instances)):
+            cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
+            cls.instances[layer].syn_pre = torch.zeros_like(cls.instances[layer].syn_pre)
+            cls.instances[layer].syn_post = torch.zeros_like(cls.instances[layer].syn_post)
+            cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
 
 # Spike-gradient functions
 
