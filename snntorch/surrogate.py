4742e4a8b84523c98dd7e046de3b37763f840a44
Jason Eshraghian <40262130+jeshraghian@users.noreply.github.com>
2021-01-17 18:25:20 +1100

    surrogate.SpikeRateEscape added --untested

--- snntorch/surrogate.py
+++ snntorch/surrogate.py
@@ -56,6 +56,34 @@ class Sigmoid(torch.autograd.Function):
         grad = grad_input * slope * torch.exp(-slope*input_) / ((torch.exp(-slope*input_)+1) ** 2)
         return grad
 
-# boltzmann func
+
+class SpikeRateEscape(torch.autograd.Function):
+    """
+    Surrogate gradient of the Heaviside step function.
+    Forward pass: Boltzmann Distribution.
+    Backward pass: Gradient of Boltzmann Distribution.
+
+    f'(x) = slope * exp(-beta*abs(x - 1))
+
+    Wulfram Gerstner and Werner M. Kistler, "Spiking neuron models: Single neurons, populations, plasticity."
+    Cambridge University Press, 2002.
+    """
+
+    def __init__(self, beta=1):
+        self.beta = beta
+
+    @staticmethod
+    def forward(ctx, input_):
+        ctx.save_for_backward(input_)
+        out = torch.zeros_like(input_)
+        out[input_ > 0] = 1.0
+        return out
+
+    def backward(self, ctx, grad_output):
+        input_, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad = grad_input * slope * torch.exp(-self.beta*torch.abs(input_ - 1))
+        return grad
+
 # piecewise linear func
 # tanh surrogate func
