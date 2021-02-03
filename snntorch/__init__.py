backprop timestep fixes and tests; spikegen tests and return arg mods

snntorch/__init__.py
@@ -170,9 +170,10 @@ class Stein(LIF):
         Intended for use in truncated backpropagation through time where hidden state variables are instance variables."""
 
         for layer in range(len(cls.instances)):
-            cls.instances[layer].spk.detach_()
-            cls.instances[layer].syn.detach_()
-            cls.instances[layer].mem.detach_()
+            if isinstance(cls.instances[layer], Stein):
+                cls.instances[layer].spk.detach_()
+                cls.instances[layer].syn.detach_()
+                cls.instances[layer].mem.detach_()
 
     @classmethod
     def zeros_hidden(cls):
@@ -180,9 +181,10 @@ class Stein(LIF):
         Intended for use where hidden state variables are instance variables."""
 
         for layer in range(len(cls.instances)):
-            cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
-            cls.instances[layer].syn = torch.zeros_like(cls.instances[layer].syn)
-            cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
+            if isinstance(cls.instances[layer], Stein):
+                cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
+                cls.instances[layer].syn = torch.zeros_like(cls.instances[layer].syn)
+                cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
 
 
 class SRM0(LIF):
@@ -232,9 +234,15 @@ class SRM0(LIF):
                     batch_size, num_inputs
                 )
 
-        self.tau_srm = np.log(self.alpha) / (np.log(self.beta) - np.log(self.alpha)) + 1
         if self.alpha <= self.beta:
             raise ValueError("alpha must be greater than beta.")
+
+        if self.beta == 1:
+            raise ValueError(
+                "beta cannot be '1' otherwise ZeroDivisionError occurs: tau_srm = log(alpha)/log(beta) - log(alpha) + 1"
+            )
+
+        self.tau_srm = np.log(self.alpha) / (np.log(self.beta) - np.log(self.alpha)) + 1
 
     def forward(self, input_, syn_pre, syn_post, mem):
         # if hidden states are passed externally
@@ -278,21 +286,23 @@ class SRM0(LIF):
         Intended for use in truncated backpropagation through
         time where hidden state variables are instance variables."""
         for layer in range(len(cls.instances)):
-            cls.instances[layer].spk.detach_()
-            cls.instances[layer].syn_pre.detach_()
-            cls.instances[layer].syn_post.detach_()
-            cls.instances[layer].mem.detach_()
+            if isinstance(cls.instances[layer], SRM0):
+                cls.instances[layer].spk.detach_()
+                cls.instances[layer].syn_pre.detach_()
+                cls.instances[layer].syn_post.detach_()
+                cls.instances[layer].mem.detach_()
 
     @classmethod
     def zeros_hidden(cls):
         """Used to clear hidden state variables to zero.
         Intended for use where hidden state variables are instance variables."""
         for layer in range(len(cls.instances)):
-            cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
-            cls.instances[layer].syn_pre = torch.zeros_like(
-                cls.instances[layer].syn_pre
-            )
-            cls.instances[layer].syn_post = torch.zeros_like(
-                cls.instances[layer].syn_post
-            )
-            cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
+            if isinstance(cls.instances[layer], SRM0):
+                cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
+                cls.instances[layer].syn_pre = torch.zeros_like(
+                    cls.instances[layer].syn_pre
+                )
+                cls.instances[layer].syn_post = torch.zeros_like(
+                    cls.instances[layer].syn_post
+                )
+                cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)

snntorch/backprop.py
@@ -2,27 +2,31 @@ import snntorch as snn
 import torch
 
 
-def BPTT(net, data, target, num_steps, batch_size, optimizer, criterion):
+def BPTT(
+    net, data, target, num_steps, batch_size, optimizer, criterion, data_time=True
+):
     """Backpropagation through time. LIF layers require parameter ``hidden_init = True``
     rather than hidden global variables. BPTT is equivalent to TBPTT for the case where
     num_steps = K.
 
-               Parameters
-               ----------
-               net : nn.Module
-                   Network model.
-               data :  torch, tensor
-                   Data tensor for a single batch.
-               target : torch tensor
-                   Target tensor for a single batch.
-               num_steps : int
-                   Number of time steps.
-               batch_size : int
-                   Number of samples in a single batch.
-               optimizer : torch.optim
+                Parameters
+                ----------
+                net : nn.Module
+                    Network model.
+                data :  torch, tensor
+                    Data tensor for a single batch.
+                target : torch tensor
+                    Target tensor for a single batch.
+                num_steps : int
+                    Number of time steps.
+                batch_size : int
+                    Number of samples in a single batch.
+                optimizer : torch.optim
                     Optimizer used, e.g., torch.optim.adam.Adam.
-               criterion : torch.nn.modules.loss
+                criterion : torch.nn.modules.loss
                     Loss criterion, e.g., torch.nn.modules.loss.CrossEntropyLoss
+                data_time : Bool
+                    True if input data is time-varying [T x B x dims] (default : ``True``).
 
                 Returns
                 -------
@@ -31,31 +35,80 @@ def BPTT(net, data, target, num_steps, batch_size, optimizer, criterion):
     """
     #  Net requires hidden instance variables rather than global instance variables for TBPTT
     return TBPTT(
-        net, data, target, num_steps, batch_size, optimizer, criterion, K=num_steps
+        net,
+        data,
+        target,
+        num_steps,
+        batch_size,
+        optimizer,
+        criterion,
+        data_time,
+        K=num_steps,
     )
 
 
-def TBPTT(net, data, target, num_steps, batch_size, optimizer, criterion, K=1):
+def RTRL(
+    net, data, target, num_steps, batch_size, optimizer, criterion, data_time=True
+):
+    """Real-time Recurrent Learning. LIF layers require parameter ``hidden_init = True``
+    rather than hidden global variables. RTRL is equivalent to TBPTT for the case where
+    K = 1.
+
+               Parameters
+               ----------
+                net : nn.Module
+                    Network model.
+                data :  torch, tensor
+                    Data tensor for a single batch.
+                target : torch tensor
+                    Target tensor for a single batch.
+                num_steps : int
+                    Number of time steps.
+                batch_size : int
+                    Number of samples in a single batch.
+                optimizer : torch.optim
+                    Optimizer used, e.g., torch.optim.adam.Adam.
+                criterion : torch.nn.modules.loss
+                    Loss criterion, e.g., torch.nn.modules.loss.CrossEntropyLoss
+                data_time : Bool
+                    True if input data is time-varying [T x B x dims] (default : ``True``).
+
+                Returns
+                -------
+                torch.Tensor
+                    average loss for a single minibatch.
+    """
+    #  Net requires hidden instance variables rather than global instance variables for TBPTT
+    return TBPTT(
+        net, data, target, num_steps, batch_size, optimizer, criterion, data_time, K=1
+    )
+
+
+def TBPTT(
+    net, data, target, num_steps, batch_size, optimizer, criterion, data_time=True, K=1
+):
     """Truncated backpropagation through time. LIF layers require parameter ``hidden_init = True``
     rather than hidden global variables.
 
                Parameters
                ----------
-               net : nn.Module
-                   Network model.
-               data :  torch, tensor
-                   Data tensor for a single batch.
-               target : torch Tensor
-                   Target tensor for a single batch.
-               num_steps : int
-                   Number of time steps.
-               batch_size : int
-                   Number of samples in a single batch.
-               optimizer : torch.optim
+                net : nn.Module
+                    Network model.
+                data :  torch, tensor
+                    Data tensor for a single batch.
+                target : torch Tensor
+                    Target tensor for a single batch.
+                num_steps : int
+                    Number of time steps.
+                batch_size : int
+                    Number of samples in a single batch.
+                optimizer : torch.optim
                     Optimizer used, e.g., torch.optim.adam.Adam.
-               criterion : torch.nn.modules.loss
+                criterion : torch.nn.modules.loss
                     Loss criterion, e.g., torch.nn.modules.loss.CrossEntropyLoss
-               K : int, optional
+                data_time : Bool
+                    True if input data is time-varying [T x B x dims] (default : ``True``).
+                K : int, optional
                     Number of time steps to process per weight update (default: ``num_steps``).
 
                 Returns
@@ -64,6 +117,9 @@ def TBPTT(net, data, target, num_steps, batch_size, optimizer, criterion, K=1):
                     average loss for a single minibatch.
     """
 
+    if K > num_steps:
+        raise ValueError("K must be less than or equal to num_steps.")
+
     _layer_init(net=net)  # Check which LIF neurons are in net. Reset and detach them.
 
     t = 0
@@ -71,7 +127,10 @@ def TBPTT(net, data, target, num_steps, batch_size, optimizer, criterion, K=1):
     loss_avg = 0
 
     for step in range(num_steps):
-        spk_out, mem_out = net(data.view(batch_size, -1))
+        if data_time:
+            spk_out, mem_out = net(data[step].view(batch_size, -1))
+        else:
+            spk_out, mem_out = net(data.view(batch_size, -1))
         loss = criterion(mem_out, target)
         loss_trunc += loss
         loss_avg += loss

snntorch/spikegen.py
@@ -45,21 +45,6 @@ def rate(
                     one-hot encoding of targets with time optionally in the first dimension.
     """
 
-    if convert_targets:
-        # One-hot encoding of targets and repeat it along the first dimension, i.e., time first.
-        spike_targets = targets_to_spikes(
-            targets, num_outputs, num_steps, temporal_targets
-        )
-
-    elif not convert_targets and temporal_targets:
-        # Repeat tensor in the first dimension without converting targets to one-hot.
-        spike_targets = targets.repeat(
-            tuple([num_steps] + torch.ones(len(targets.size()), dtype=int).tolist())
-        )
-
-    else:
-        spike_targets = targets
-
     # Generate a tuple: (1, num_steps, 1..., 1) where the number of 1's = number of dimensions in the original data.
     # Multiply by gain and add offset.
     time_data = (
@@ -72,7 +57,22 @@ def rate(
 
     spike_data = rate_conv(time_data)
 
-    return spike_data, spike_targets
+    if convert_targets:
+        # One-hot encoding of targets and repeat it along the first dimension, i.e., time first.
+        spike_targets = targets_to_spikes(
+            targets, num_outputs, num_steps, temporal_targets
+        )
+        return spike_data, spike_targets
+
+    elif not convert_targets and temporal_targets:
+        # Repeat tensor in the first dimension without converting targets to one-hot.
+        spike_targets = targets.repeat(
+            tuple([num_steps] + torch.ones(len(targets.size()), dtype=int).tolist())
+        )
+        return spike_data, spike_targets
+
+    else:
+        return spike_data
 
 
 def latency(
@@ -130,21 +130,6 @@ def latency(
                     one-hot encoding of targets with time optionally in the first dimension.
     """
 
-    if convert_targets:
-        # One-hot encoding of targets and repeat it along the first dimension, i.e., time first.
-        spike_targets = targets_to_spikes(
-            targets, num_outputs, num_steps, temporal_targets
-        )
-
-    elif not convert_targets and temporal_targets:
-        # Repeat tensor in the first dimension without converting targets to one-hot.
-        spike_targets = targets.repeat(
-            tuple([num_steps] + torch.ones(len(targets.size()), dtype=int).tolist())
-        )
-
-    else:
-        spike_targets = targets
-
     spike_data = latency_conv(
         data,
         num_steps=num_steps,
@@ -156,7 +141,22 @@ def latency(
         linear=linear,
     )
 
-    return spike_data, spike_targets
+    if convert_targets:
+        # One-hot encoding of targets and repeat it along the first dimension, i.e., time first.
+        spike_targets = targets_to_spikes(
+            targets, num_outputs, num_steps, temporal_targets
+        )
+        return spike_data, spike_targets
+
+    elif not convert_targets and temporal_targets:
+        # Repeat tensor in the first dimension without converting targets to one-hot.
+        spike_targets = targets.repeat(
+            tuple([num_steps] + torch.ones(len(targets.size()), dtype=int).tolist())
+        )
+        return spike_data, spike_targets
+
+    else:
+        return spike_data
 
 
 def rate_conv(data):