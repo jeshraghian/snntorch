c322eda1538a60fc6026752cad0c1df1c789adb8
jeshraghian <40262130+jeshraghian@users.noreply.github.com>
2021-02-11 17:33:33 +1100

    verstr applied to version

--- setup.py
+++ setup.py
@@ -73,6 +73,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/jeshraghian/snntorch",
-    version="0.1.0",
+    version=verstr,
     zip_safe=False,
 )
