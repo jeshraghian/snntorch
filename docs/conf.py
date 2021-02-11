5154e3d542bca82675c0921347e49cd1cfbc80b8
jeshraghian <40262130+jeshraghian@users.noreply.github.com>
2021-02-11 15:57:22 +1100

    logo update, font family update docs

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ html_theme = "sphinx_rtd_theme"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/img/snntorch_logo_alpha.png"
+html_logo = "_static/img/snntorch_alpha.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
