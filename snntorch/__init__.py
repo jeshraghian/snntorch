fe72f743fa44147f9fb3ba5bb7587ec44a26b9d2
Jason Eshraghian <jeshraghian@gmail.com>
2021-06-11 13:51:13 +1000

    neuron model Stein deprecation msg added

--- snntorch/__init__.py
+++ snntorch/__init__.py
@@ -320,7 +320,6 @@ class LIF1(LIF):
                 cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
 
 
-
 class LIF2(LIF):
     """
     Conductance-based model of the leaky integrate and fire neuron.
@@ -482,173 +481,6 @@ class LIF2(LIF):
 
         for layer in range(len(cls.instances)):
             if isinstance(cls.instances[layer], LIF2):
-                cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
-                cls.instances[layer].syn = torch.zeros_like(cls.instances[layer].syn)
-                cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
-
-class Stein(LIF):
-    """
-    Stein's model of the leaky integrate and fire neuron.
-    The synaptic current jumps upon spike arrival, which causes a jump in membrane potential.
-    Synaptic current and membrane potential decay exponentially with rates of alpha and beta, respectively.
-    For :math:`U[T] > U_{\\rm thr} ⇒ S[T+1] = 1`.
-
-    If `reset_mechanism = "subtract"`, then :math:`U[t+1]` will have `threshold` subtracted from it whenever the neuron emits a spike:
-
-    .. math::
-
-            I_{\\rm syn}[t+1] = αI_{\\rm syn}[t] + I_{\\rm in}[t+1] \\\\
-            U[t+1] = βU[t] + I_{\\rm syn}[t+1] - RU_{\\rm thr}
-
-    If `reset_mechanism = "zero"`, then :math:`U[t+1]` will be set to `0` whenever the neuron emits a spike:
-
-    .. math::
-
-            I_{\\rm syn}[t+1] = αI_{\\rm syn}[t] + I_{\\rm in}[t+1] \\\\
-            U[t+1] = βU[t] + I_{\\rm syn}[t+1] - R(βU[t] + I_{\\rm syn}[t+1])
-
-    * :math:`I_{\\rm syn}` - Synaptic current
-    * :math:`I_{\\rm in}` - Input current
-    * :math:`U` - Membrane potential
-    * :math:`U_{\\rm thr}` - Membrane threshold
-    * :math:`R` - Reset mechanism: if active, :math:`R = 1`, otherwise :math:`R = 0`
-    * :math:`α` - Synaptic current decay rate
-    * :math:`β` - Membrane potential decay rate
-
-    Example::
-
-        import torch
-        import torch.nn as nn
-        import snntorch as snn
-
-        alpha = 0.9
-        beta = 0.5
-
-        # Define Network
-        class Net(nn.Module):
-            def __init__(self):
-                super().__init__()
-
-                # initialize layers
-                self.fc1 = nn.Linear(num_inputs, num_hidden)
-                self.lif1 = snn.Stein(alpha=alpha, beta=beta)
-                self.fc2 = nn.Linear(num_hidden, num_outputs)
-                self.lif2 = snn.Stein(alpha=alpha, beta=beta)
-
-            def forward(self, x, syn1, mem1, spk1, syn2, mem2):
-                cur1 = self.fc1(x)
-                spk1, syn1, mem1 = self.lif1(cur1, syn1, mem1)
-                cur2 = self.fc2(spk1)
-                spk2, syn2, mem2 = self.lif2(cur2, syn2, mem2)
-                return syn1, mem1, spk1, syn2, mem2, spk2
-
-
-    For further reading, see:
-
-    *R. B. Stein (1965) A theoretical analysis of neuron variability. Biophys. J. 5, pp. 173-194.*
-
-    *R. B. Stein (1967) Some models of neuronal variability. Biophys. J. 7. pp. 37-68.*"""
-
-    def __init__(
-        self,
-        alpha,
-        beta,
-        threshold=1.0,
-        num_inputs=False,
-        spike_grad=None,
-        batch_size=False,
-        hidden_init=False,
-        inhibition=False,
-        reset_mechanism="subtract",
-    ):
-        super(Stein, self).__init__(
-            beta, threshold, spike_grad, inhibition, reset_mechanism
-        )
-
-        print("`Stein` has been deprecated and will be removed in a future version. Use `LIF2` instead.")
-
-        self.alpha = alpha
-        self.num_inputs = num_inputs
-        self.batch_size = batch_size
-        self.hidden_init = hidden_init
-
-        if self.hidden_init:
-            if not self.num_inputs:
-                raise ValueError(
-                    "num_inputs must be specified to initialize hidden states as instance variables."
-                )
-            elif not self.batch_size:
-                raise ValueError(
-                    "batch_size must be specified to initialize hidden states as instance variables."
-                )
-            elif hasattr(self.num_inputs, "__iter__"):
-                self.spk, self.syn, self.mem = self.init_stein(
-                    self.batch_size, *(self.num_inputs)
-                )  # need to automatically call batch_size
-            else:
-                self.spk, self.syn, self.mem = self.init_stein(
-                    self.batch_size, self.num_inputs
-                )
-        if self.inhibition:
-            if not self.batch_size:
-                raise ValueError(
-                    "batch_size must be specified to enable firing inhibition."
-                )
-
-    def forward(self, input_, syn, mem):
-        if not self.hidden_init:
-            if self.inhibition:
-                spk, reset = self.fire_inhibition(self.batch_size, mem)
-            else:
-                spk, reset = self.fire(mem)
-            syn = self.alpha * syn + input_
-
-            if self.reset_mechanism == "subtract":
-                mem = self.beta * mem + syn - reset * self.threshold
-
-            elif self.reset_mechanism == "zero":
-                mem = self.beta * mem + syn - reset * (self.beta * mem + syn)
-
-            return spk, syn, mem
-
-        # intended for truncated-BPTT where instance variables are hidden states
-        if self.hidden_init:
-            if self.inhibition:
-                self.spk, self.reset = self.fire_inhibition(self.batch_size, self.mem)
-            else:
-                self.spk, self.reset = self.fire(self.mem)
-            self.syn = self.alpha * self.syn + input_
-
-            if self.reset_mechanism == "subtract":
-                self.mem = self.beta * self.mem + self.syn - self.reset * self.threshold
-
-            elif self.reset_mechanism == "zero":
-                self.mem = (
-                    self.beta * self.mem
-                    + self.syn
-                    - self.reset * (self.beta * self.mem + self.syn)
-                )
-
-            return self.spk, self.syn, self.mem
-
-    @classmethod
-    def detach_hidden(cls):
-        """Returns the hidden states, detached from the current graph.
-        Intended for use in truncated backpropagation through time where hidden state variables are instance variables."""
-
-        for layer in range(len(cls.instances)):
-            if isinstance(cls.instances[layer], Stein):
-                cls.instances[layer].spk.detach_()
-                cls.instances[layer].syn.detach_()
-                cls.instances[layer].mem.detach_()
-
-    @classmethod
-    def zeros_hidden(cls):
-        """Used to clear hidden state variables to zero.
-        Intended for use where hidden state variables are instance variables."""
-
-        for layer in range(len(cls.instances)):
-            if isinstance(cls.instances[layer], Stein):
                 cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
                 cls.instances[layer].syn = torch.zeros_like(cls.instances[layer].syn)
                 cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
@@ -1132,4 +964,172 @@ class SRM0(LIF):
                 cls.instances[layer].syn_post = torch.zeros_like(
                     cls.instances[layer].syn_post
                 )
+                cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
+
+
+class Stein(LIF):
+    """
+    Stein's model of the leaky integrate and fire neuron.
+    The synaptic current jumps upon spike arrival, which causes a jump in membrane potential.
+    Synaptic current and membrane potential decay exponentially with rates of alpha and beta, respectively.
+    For :math:`U[T] > U_{\\rm thr} ⇒ S[T+1] = 1`.
+
+    If `reset_mechanism = "subtract"`, then :math:`U[t+1]` will have `threshold` subtracted from it whenever the neuron emits a spike:
+
+    .. math::
+
+            I_{\\rm syn}[t+1] = αI_{\\rm syn}[t] + I_{\\rm in}[t+1] \\\\
+            U[t+1] = βU[t] + I_{\\rm syn}[t+1] - RU_{\\rm thr}
+
+    If `reset_mechanism = "zero"`, then :math:`U[t+1]` will be set to `0` whenever the neuron emits a spike:
+
+    .. math::
+
+            I_{\\rm syn}[t+1] = αI_{\\rm syn}[t] + I_{\\rm in}[t+1] \\\\
+            U[t+1] = βU[t] + I_{\\rm syn}[t+1] - R(βU[t] + I_{\\rm syn}[t+1])
+
+    * :math:`I_{\\rm syn}` - Synaptic current
+    * :math:`I_{\\rm in}` - Input current
+    * :math:`U` - Membrane potential
+    * :math:`U_{\\rm thr}` - Membrane threshold
+    * :math:`R` - Reset mechanism: if active, :math:`R = 1`, otherwise :math:`R = 0`
+    * :math:`α` - Synaptic current decay rate
+    * :math:`β` - Membrane potential decay rate
+
+    Example::
+
+        import torch
+        import torch.nn as nn
+        import snntorch as snn
+
+        alpha = 0.9
+        beta = 0.5
+
+        # Define Network
+        class Net(nn.Module):
+            def __init__(self):
+                super().__init__()
+
+                # initialize layers
+                self.fc1 = nn.Linear(num_inputs, num_hidden)
+                self.lif1 = snn.Stein(alpha=alpha, beta=beta)
+                self.fc2 = nn.Linear(num_hidden, num_outputs)
+                self.lif2 = snn.Stein(alpha=alpha, beta=beta)
+
+            def forward(self, x, syn1, mem1, spk1, syn2, mem2):
+                cur1 = self.fc1(x)
+                spk1, syn1, mem1 = self.lif1(cur1, syn1, mem1)
+                cur2 = self.fc2(spk1)
+                spk2, syn2, mem2 = self.lif2(cur2, syn2, mem2)
+                return syn1, mem1, spk1, syn2, mem2, spk2
+
+
+    For further reading, see:
+
+    *R. B. Stein (1965) A theoretical analysis of neuron variability. Biophys. J. 5, pp. 173-194.*
+
+    *R. B. Stein (1967) Some models of neuronal variability. Biophys. J. 7. pp. 37-68.*"""
+
+    def __init__(
+        self,
+        alpha,
+        beta,
+        threshold=1.0,
+        num_inputs=False,
+        spike_grad=None,
+        batch_size=False,
+        hidden_init=False,
+        inhibition=False,
+        reset_mechanism="subtract",
+    ):
+        super(Stein, self).__init__(
+            beta, threshold, spike_grad, inhibition, reset_mechanism
+        )
+
+        print("`Stein` has been deprecated and will be removed in a future version. Use `LIF2` instead.")
+
+        self.alpha = alpha
+        self.num_inputs = num_inputs
+        self.batch_size = batch_size
+        self.hidden_init = hidden_init
+
+        if self.hidden_init:
+            if not self.num_inputs:
+                raise ValueError(
+                    "num_inputs must be specified to initialize hidden states as instance variables."
+                )
+            elif not self.batch_size:
+                raise ValueError(
+                    "batch_size must be specified to initialize hidden states as instance variables."
+                )
+            elif hasattr(self.num_inputs, "__iter__"):
+                self.spk, self.syn, self.mem = self.init_stein(
+                    self.batch_size, *(self.num_inputs)
+                )  # need to automatically call batch_size
+            else:
+                self.spk, self.syn, self.mem = self.init_stein(
+                    self.batch_size, self.num_inputs
+                )
+        if self.inhibition:
+            if not self.batch_size:
+                raise ValueError(
+                    "batch_size must be specified to enable firing inhibition."
+                )
+
+    def forward(self, input_, syn, mem):
+        if not self.hidden_init:
+            if self.inhibition:
+                spk, reset = self.fire_inhibition(self.batch_size, mem)
+            else:
+                spk, reset = self.fire(mem)
+            syn = self.alpha * syn + input_
+
+            if self.reset_mechanism == "subtract":
+                mem = self.beta * mem + syn - reset * self.threshold
+
+            elif self.reset_mechanism == "zero":
+                mem = self.beta * mem + syn - reset * (self.beta * mem + syn)
+
+            return spk, syn, mem
+
+        # intended for truncated-BPTT where instance variables are hidden states
+        if self.hidden_init:
+            if self.inhibition:
+                self.spk, self.reset = self.fire_inhibition(self.batch_size, self.mem)
+            else:
+                self.spk, self.reset = self.fire(self.mem)
+            self.syn = self.alpha * self.syn + input_
+
+            if self.reset_mechanism == "subtract":
+                self.mem = self.beta * self.mem + self.syn - self.reset * self.threshold
+
+            elif self.reset_mechanism == "zero":
+                self.mem = (
+                    self.beta * self.mem
+                    + self.syn
+                    - self.reset * (self.beta * self.mem + self.syn)
+                )
+
+            return self.spk, self.syn, self.mem
+
+    @classmethod
+    def detach_hidden(cls):
+        """Returns the hidden states, detached from the current graph.
+        Intended for use in truncated backpropagation through time where hidden state variables are instance variables."""
+
+        for layer in range(len(cls.instances)):
+            if isinstance(cls.instances[layer], Stein):
+                cls.instances[layer].spk.detach_()
+                cls.instances[layer].syn.detach_()
+                cls.instances[layer].mem.detach_()
+
+    @classmethod
+    def zeros_hidden(cls):
+        """Used to clear hidden state variables to zero.
+        Intended for use where hidden state variables are instance variables."""
+
+        for layer in range(len(cls.instances)):
+            if isinstance(cls.instances[layer], Stein):
+                cls.instances[layer].spk = torch.zeros_like(cls.instances[layer].spk)
+                cls.instances[layer].syn = torch.zeros_like(cls.instances[layer].syn)
                 cls.instances[layer].mem = torch.zeros_like(cls.instances[layer].mem)
