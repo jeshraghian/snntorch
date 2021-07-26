a261a944df30b267dce4cd6663a1650f1bea66a6
Jason Eshraghian <jeshraghian@gmail.com>
2021-07-27 08:11:46 +1000

    Bump version: 0.4.3 → 0.4.4

--- _version.py
+++ _version.py
@@ -1,2 +1,2 @@
 # fmt: off
-__version__ = '0.4.3'
+__version__ = '0.4.4'
--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath(".."))
 import snntorch
 
 # fmt: off
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 # fmt: on
 
 
