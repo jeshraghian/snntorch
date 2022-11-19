dbca83049bcff04604d5b04c3f977652bfd897dc
jasonesh <jeshraghian@gmail.com>
2022-11-18 19:25:22 -0800

    py310 update setup

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
     description="Deep learning with spiking neural networks.",
