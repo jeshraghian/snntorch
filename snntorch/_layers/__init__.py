935e0e57a6764120e49f5b0e4f5458cfdf1b6278
Vincent Sun <vincentpssun@gmail.com>
2022-11-24 11:43:35 +0800

    updated snntorch/_layers/__init__.py

--- snntorch/_layers/__init__.py
+++ snntorch/_layers/__init__.py
@@ -1,16 +1,6 @@
-###############################################################
-# When adding new neurons, update the following:              #
-# i) create neuron in snntorch/_neurons/your_neuron.py        #
-# ii) utils.py: reset(); _layer_check(), _layer_reset() etc.  #
-# iii) neurons_dict in backprop.py,                           #
-# iv) update __neuron__ & import below                        #
-# v) unit tests in tests/test_snntorch.py                     #
-# vi) update docs: snntorch.rst                               #
-###############################################################
-
 __layer__ = [
-    "bntt1d",
-    "bntt2d"
+    "BatchNormTT1d",
+    "BatchNormTT2d"
 ]
 
 from .bntt import *
