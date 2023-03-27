3f85560783a1298178a9ae1c2fe0dd9a6eb39402
jasonesh <jeshraghian@gmail.com>
2023-03-26 15:50:20 -1000

    Bump version: 0.6.1 → 0.6.2

--- _version.py
+++ _version.py
@@ -1,2 +1,2 @@
 # fmt: off
-__version__ = '0.6.1'
+__version__ = '0.6.2'
--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 
 # fmt: off
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 # fmt: on
 
 
