08ab18fcd5685d678217bfaa8de734dfd828f790
unknown <jeshraghian@gmail.com>
2024-04-18 14:14:16 -0700

    Bump version: 0.8.2 → 0.8.3

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 
 # fmt: off
-__version__ = '0.8.2'
+__version__ = '0.8.3'
 # fmt: on
 
 
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst", encoding="utf-8") as readme_file:
 #     history = history_file.read()
 
 # fmt: off
-__version__ = '0.8.2'
+__version__ = '0.8.3'
 # fmt: on
 
 requirements = [
