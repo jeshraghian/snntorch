f4d3ba84e5d4ea12bdcf635c836d1ceecc968878
Andrew Smith <andrew.smith.recruiting@gmail.com>
2024-12-03 22:33:09 -0800

    cleanup bench: perf comparison and tinystories

--- snntorch/_neurons/stateleaky.py
+++ snntorch/_neurons/stateleaky.py
@@ -24,9 +24,9 @@ class StateLeaky(LIF):
         surrogate_disable=False,
         learn_beta=False,
         learn_threshold=False,
-        state_quant=False, 
+        state_quant=False,
         output=True,
-        graded_spikes_factor=1.0, 
+        graded_spikes_factor=1.0,
         learn_graded_spikes_factor=False,
     ):
         super().__init__(
@@ -45,12 +45,11 @@ class StateLeaky(LIF):
         self._tau_buffer(self.beta, learn_beta)
 
     @property
-    def beta(self): 
-        return (self.tau-1) / self.tau
+    def beta(self):
+        return (self.tau - 1) / self.tau
 
     # @profile(skip=True, stdout=True, filename='baseline.prof')
     def forward(self, input_):
-        print(input_.shape)
         self.mem = self._base_state_function(input_)
 
         if self.state_quant:
@@ -71,12 +70,12 @@ class StateLeaky(LIF):
         time_steps = time_steps.unsqueeze(1).expand(num_steps, channels)
 
         # init decay filter
-        print(time_steps.shape)
-        print(self.tau.shape)
+        # print(time_steps.shape)
+        # print(self.tau.shape)
         decay_filter = torch.exp(-time_steps / self.tau).to(input_.device)
-        print("------------decay filter------------")
-        print(decay_filter)
-        print()
+        # print("------------decay filter------------")
+        # print(decay_filter)
+        # print()
         assert decay_filter.shape == (num_steps, channels)
 
         # prepare for convolution
@@ -89,18 +88,18 @@ class StateLeaky(LIF):
         assert conv_result.shape == (batch, channels, num_steps)
 
         return conv_result.permute(2, 0, 1)  # return membrane potential trace
-    
+
     def _tau_buffer(self, beta, learn_beta):
         if not isinstance(beta, torch.Tensor):
             beta = torch.as_tensor(beta)
-        
+
         tau = 1 / (1 - beta + 1e-12)
 
         if learn_beta:
             self.tau = nn.Parameter(tau)
         else:
             self.register_buffer("tau", tau)
-    
+
     def full_mode_conv1d_truncated(self, input_tensor, kernel_tensor):
         # input_tensor: (batch, channels, num_steps)
         # kernel_tensor: (channels, 1, kernel_size)
@@ -132,6 +131,7 @@ class StateLeaky(LIF):
 # fire_inhibition
 # mem_reset, init, detach, zeros, reset_mem, init_leaky
 # detach_hidden, reset_hidden
+
 
 if __name__ == "__main__":
     device = "cuda"
--- tests/test_snntorch/test_stateleaky.py
+++ tests/test_snntorch/test_stateleaky.py
@@ -1,78 +1,68 @@
 import pytest
 import torch
-from snntorch._neurons.stateleaky import StateLeaky  # Adjust the import path as needed
+from snntorch._neurons.stateleaky import StateLeaky
 
 
 @pytest.fixture(scope="module")
 def device():
-    # Fixture to use GPU if available, otherwise fall back to CPU
     return "cuda" if torch.cuda.is_available() else "cpu"
 
 
 @pytest.fixture(scope="module")
 def linear_leaky_instance(device):
-    # Fixture to initialize the StateLeaky instance
     return StateLeaky(beta=0.9, output=False).to(device)
+
 
 @pytest.fixture(scope="module")
 def linear_leaky_instance_multi_beta(device):
-    # Fixture to initialize the StateLeaky instance
     return StateLeaky(beta=torch.tensor([0.9, 0.9, 0.9, 0.9]), output=False).to(device)
+
 
 @pytest.fixture(scope="module")
 def input_tensor_single_batch_single_channel(device):
-    # Define the shape parameters
     timesteps = 5
     batch = 1
     channels = 1
-    
-    # Create a sequence for each timestep and expand along batch and channel dimensions
-    input_ = torch.arange(0, timesteps).float().view(timesteps, 1, 1) / timesteps  # Shape (timesteps, 1, 1)
-    input_ = input_.expand(timesteps, batch, channels)  # Expand to (timesteps, batch, channels)
-    
-    # Move to the specified device (e.g., CPU or GPU)
+
+    input_ = torch.arange(0, timesteps).float().view(timesteps, 1, 1) / timesteps
+    input_ = input_.expand(timesteps, batch, channels)
+
     return input_.to(device)
+
 
 @pytest.fixture(scope="module")
 def input_tensor_single_batch_multiple_channel(device):
-    # Define the shape parameters
     timesteps = 5
     batch = 1
     channels = 4
-    
-    # Create a sequence for each timestep and expand along batch and channel dimensions
-    input_ = torch.arange(0, timesteps).float().view(timesteps, 1, 1) / timesteps  # Shape (timesteps, 1, 1)
-    input_ = input_.expand(timesteps, batch, channels)  # Expand to (timesteps, batch, channels)
-    
-    # Move to the specified device (e.g., CPU or GPU)
+
+    input_ = torch.arange(0, timesteps).float().view(timesteps, 1, 1) / timesteps
+    input_ = input_.expand(timesteps, batch, channels)
+
     return input_.to(device)
+
 
 @pytest.fixture(scope="module")
 def input_tensor_multiple_batches_single_channel(device):
-    # Define the shape parameters
     timesteps = 5
     batch = 2
     channels = 1
-    
-    # Create a sequence for each timestep and expand along batch and channel dimensions
-    input_ = torch.arange(0, timesteps).float().view(timesteps, 1, 1) / timesteps  # Shape (timesteps, 1, 1)
-    input_ = input_.expand(timesteps, batch, channels)  # Expand to (timesteps, batch, channels)
-    
-    # Move to the specified device (e.g., CPU or GPU)
+
+    input_ = torch.arange(0, timesteps).float().view(timesteps, 1, 1) / timesteps
+    input_ = input_.expand(timesteps, batch, channels)
+
     return input_.to(device)
+
 
 @pytest.fixture(scope="module")
 def input_tensor_batch_multiple(device):
-    # Define the shape parameters
     timesteps = 5
     batch = 2
     channels = 4
-    
-    # Create a sequence for each timestep and expand along batch and channel dimensions
-    input_ = torch.arange(0, timesteps).float().view(timesteps, 1, 1) / timesteps  # Shape (timesteps, 1, 1)
-    input_ = input_.expand(timesteps, batch, channels)  # Expand to (timesteps, batch, channels)
-    
-    # Move to the specified device (e.g., CPU or GPU)
+
+    input_ = torch.arange(0, timesteps).float().view(timesteps, 1, 1) / timesteps
+    input_ = input_.expand(timesteps, batch, channels)
+
     return input_.to(device)
 
 
@@ -82,15 +72,12 @@ def test_forward_method_correctness_single_batch_single_channel(
 
     assert input_tensor_single_batch_single_channel.shape == output.shape
 
-    # Ensure all input elements are <= 1
     assert (input_tensor_single_batch_single_channel <= 1).all().item(), \
         "Some input elements are greater than 1."
 
-    # Ensure the output shape matches the input shape
     assert output.shape == input_tensor_single_batch_single_channel.shape, \
         "Output shape does not match input shape."
 
-    # Check if there are some elements in the output that are > 1
     assert (output > 1).any().item(), \
         "No elements in the output are greater than 1."
 
@@ -104,15 +91,12 @@ def test_forward_method_correctness_multiple_batches_multiple_channels(
 
     print("input_tensor_batch_multiple: ", input_tensor_batch_multiple)
 
-    # Ensure all input elements are <= 1
     assert (input_tensor_batch_multiple <= 1).all().item(), \
         "Some input elements are greater than 1."
 
-    # Ensure the output shape matches the input shape
     assert output.shape == input_tensor_batch_multiple.shape, \
         "Output shape does not match input shape."
 
-    # Check if there are some elements in the output that are > 1
     assert (output > 1).any().item(), \
         "No elements in the output are greater than 1."
 
@@ -125,15 +109,12 @@ def test_forward_method_correctness_multiple_batches_single_channel(
 
     print("input_tensor_batch_multiple: ", input_tensor_multiple_batches_single_channel)
 
-    # Ensure all input elements are <= 1
     assert (input_tensor_multiple_batches_single_channel <= 1).all().item(), \
         "Some input elements are greater than 1."
 
-    # Ensure the output shape matches the input shape
     assert output.shape == input_tensor_multiple_batches_single_channel.shape, \
         "Output shape does not match input shape."
 
-    # Check if there are some elements in the output that are > 1
     assert (output > 1).any().item(), \
         "No elements in the output are greater than 1."
 
@@ -144,17 +125,15 @@ def test_forward_method_correctness_single_batch_multiple_channel(
 
     assert input_tensor_single_batch_multiple_channel.shape == output.shape
 
-    # Ensure all input elements are <= 1
     assert (input_tensor_single_batch_multiple_channel <= 1).all().item(), \
         "Some input elements are greater than 1."
 
-    # Ensure the output shape matches the input shape
     assert output.shape == input_tensor_single_batch_multiple_channel.shape, \
         "Output shape does not match input shape."
 
-    # Check if there are some elements in the output that are > 1
     assert (output > 1).any().item(), \
         "No elements in the output are greater than 1."
+
 
 if __name__ == "__main__":
     pytest.main()
